f7dff3e81387adf1a34cfe4b3aa0db7e70b808b7
Zexal0807 <gallinar00@gmail.com>
2019-03-19 18:09:15 +0100

    Fix ZView

--- class/ZView/ZView.php
+++ class/ZView/ZView.php
@@ -7,11 +7,20 @@ class ZView{
 	private $html;
 	private $dir;
 
-	public function __construct($name, $data = null){
+	public function __construct($name, $base = null, $data = null){
+		if(!isset($base)){
+			$base = "";
+		}
+		if(!isset($data)){
+			$data = [];
+		}
 		$this->dir = dirname((new ReflectionClass(get_called_class()))->getFilename());
 		$this->html = file_get_contents($this->dir . DIRECTORY_SEPARATOR .static::$appFile);
 		preg_match_all('/\@include\([\'"]{1}([a-zA-Z\\\'"_]*)[\'"]{1}\)/', $this->html, $match);
 		$this->loadContent($name, $match);
+		if(find('@base', $this->html) >= 0){
+			$this->html = str_replace('@base', ($base == "" ? "" : '<base href="'.$base.'" />'), $this->html);
+		}
 		while(preg_match('/\@yield\([\'"]{1}([a-zA-Z\\\'"_]*)[\'"]{1}\)/', $this->html, $match)){
 			$this->html = str_replace($match[0], $this->loadYield($match[1]), $this->html);
 		}
