4d0e795910792621b5a442360279a4b60685e382
Zexal0807 <gallinar00@gmail.com>
2019-04-05 17:56:48 +0200

    Add method' signature open

--- class/ZSession/SessionHandle.php
+++ class/ZSession/SessionHandle.php
@@ -6,5 +6,13 @@ class SessionHandle{
   public function __construct(){
     $this->conn = new DatabaseSession();
   }
+
+  /*
+  The open callback works like a constructor in classes and is executed when the session is being opened. It is the first callback function executed when the session is started automatically or manually with session_start(). Return value is TRUE for success, FALSE for failure.
+  */
+  public function open($savePath, $sessionName){
+
+  }
+
 }
 ?>
