4e923e968970e4a713bea96aaebf88f90351d0de
Zexal0807 <gallinar00@gmail.com>
2019-04-05 14:14:49 +0200

    Set constructor and config

--- class/ZSession/DatabaseSession.php
+++ class/ZSession/DatabaseSession.php
@@ -1,3 +1,10 @@
 <?php
 class DatabaseSession{
+  public function __construct(){
+    $this->host = ZConfig::config("SESSION_DB_HOST", "localhost");
+    $this->user = ZConfig::config("SESSION_DB_USER", "root");
+    $this->password = ZConfig::config("SESSION_DB_PASSWORD", "");
+    $this->database = ZConfig::config("SESSION_DB_DATABASE", "my_database");
+    super::__construct();
+  }
 }
