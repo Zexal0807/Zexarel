ae583762b4e172ac30aa270176cbd3a767ebb44f
Zexal0807 <gallinar00@gmail.com>
2019-04-06 11:26:14 +0200

    Add open method

--- class/ZSession/SessionHandle.php
+++ class/ZSession/SessionHandle.php
@@ -23,6 +23,15 @@ class SessionHandle{
   The open callback works like a constructor in classes and is executed when the session is being opened. It is the first callback function executed when the session is started automatically or manually with session_start(). Return value is TRUE for success, FALSE for failure.
   */
   public function open($savePath, $sessionName){
+    $limit = time() - (3600 * 24 * 7);
+    $this->conn
+      ->delete()
+      ->from(ZConfig::config("SESSION_DB_TABLE", "session"))
+      ->where("time", "<", $limit)
+      ->execute(null, function($sql, $result, $row){
+        return $result;
+      });
+  }
 
   /*
   The close callback works like a destructor in classes and is executed after the session write callback has been called. It is also invoked when session_write_close() is called. Return value should be TRUE for success, FALSE for failure.
