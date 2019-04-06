836413927cc486e71afc888fbdf49b0ccbf948b2
Zexal0807 <gallinar00@gmail.com>
2019-04-06 11:00:53 +0200

    Fix DatabaseSession

--- class/ZSession/DatabaseSession.php
+++ class/ZSession/DatabaseSession.php
@@ -1,10 +1,12 @@
 <?php
-class DatabaseSession{
+class DatabaseSession extends ZDatabase{
   public function __construct(){
     $this->host = ZConfig::config("SESSION_DB_HOST", "localhost");
     $this->user = ZConfig::config("SESSION_DB_USER", "root");
     $this->password = ZConfig::config("SESSION_DB_PASSWORD", "");
     $this->database = ZConfig::config("SESSION_DB_DATABASE", "my_database");
-    super::__construct();
+    parent::__construct();
+  }
+  public function __destruct(){
   }
 }
