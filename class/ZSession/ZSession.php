f842cdaae2a23a5fd7d50ab6232158c55551d40a
Zexal0807 <gallinar00@gmail.com>
2019-04-06 15:20:34 +0200

    Add start method

--- class/ZSession/ZSession.php
+++ class/ZSession/ZSession.php
@@ -1,3 +1,15 @@
 <?php
 class ZSession{
+  public static function start(){
+    $session = new SessionHandle();
+    session_set_save_handler(
+      [$session, 'open'],
+      [$session, 'close'],
+      [$session, 'read'],
+      [$session, 'write'],
+      [$session, 'destroy'],
+      [$session, 'gc']
+    );
+    session_start();
+  }
 }
