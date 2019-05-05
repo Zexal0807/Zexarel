0d346cfc9cbccf81bec69f5557979f334322e96d
Zexal0807 <gallinar00@gmail.com>
2019-05-05 16:25:30 +0200

    Update ZModel

--- class/ZView/ZModel.php
+++ class/ZView/ZModel.php
@@ -32,17 +32,7 @@ class ZModel{
 		legge il file del modello, sostituisce i valori con quelli dei parametri, esegue la funzione eval sul risultato e ritorna l'html generato
 		*/
 		$str = file_get_contents($this->dir.$this->name.".html");
-		$str = preg_replace('/@for(.*)/', '<?php for$1{ ?>', $str);
-		$str = preg_replace('/@endfor/', '<?php } ?>', $str);
-		$str = preg_replace('/@if(.*)/', '<?php if$1{ ?>', $str);
-		$str = preg_replace('/@elseif(.*)/', '<?php }else if$1{ ?>', $str);
-		$str = preg_replace('/@else/', '<?php }else{ ?>', $str);
-		$str = preg_replace('/@endif/', '<?php } ?>', $str);
-		$str = preg_replace('/{{ \$(\S*) }}/', '<?php echo $this->property["$1"]; ?>', $str);
-		ob_start();
-		eval("?>".$str);
-		$html = ob_get_contents();
-		ob_end_clean();
+		$html = ZBladeCompiler::compile($str, $this->property);
 		return $html;
 	}
 }
