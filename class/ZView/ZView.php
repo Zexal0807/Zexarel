9231391477310d2a34e33d8969c857c5d7fb515a
Zexal0807 <gallinar00@gmail.com>
2019-05-06 15:23:20 +0200

    Fix problem

--- class/ZView/ZView.php
+++ class/ZView/ZView.php
@@ -4,50 +4,42 @@ class ZView{
 	protected static $viewDir = "view";
 	protected static $appFile = "app.html";
 
-	private $html;
-	private $dir;
+	private static $html;
+	private static $dir;
 
-	public function __construct($name, $base = null, $data = null){
+	private static function loadContent($name, $match){
+		$tmp = file_get_contents(ZView::$dir . DIRECTORY_SEPARATOR . "../" . DIRECTORY_SEPARATOR . static::$viewDir . DIRECTORY_SEPARATOR .$name.(find(".", $name) < 0 ?".html" : ""));
+		for($i = 0; $i < sizeof($match[1]); $i++){
+			$t = strlen(ZView::$html);
+			ZView::$html = str_replace("@include('".$match[1][$i]."')", get_string_between($tmp, "@".$match[1][$i], "@end".$match[1][$i]), ZView::$html);
+			if($t == strlen(ZView::$html)){
+				ZView::$html = str_replace('@include("'.$match[1][$i].'")', get_string_between($tmp, "@".$match[1][$i], "@end".$match[1][$i]), ZView::$html);
+			}
+		}
+	}
+
+	private static function loadYield($name){
+		return file_get_contents(ZView::$dir . DIRECTORY_SEPARATOR . "../" . DIRECTORY_SEPARATOR . static::$viewDir . DIRECTORY_SEPARATOR .$name.(find(".", $name) < 0 ? ".html" : ""));
+	}
+
+	public static function get($content, $base = null, $data = null){
 		if(!isset($base)){
 			$base = "";
 		}
 		if(!isset($data)){
 			$data = [];
 		}
-		$this->dir = dirname((new ReflectionClass(get_called_class()))->getFilename());
-		$this->html = file_get_contents($this->dir . DIRECTORY_SEPARATOR .static::$appFile);
-		preg_match_all('/\@include\([\'"]{1}([a-zA-Z\\\'"_]*)[\'"]{1}\)/', $this->html, $match);
-		$this->loadContent($name, $match);
-		if(find('@base', $this->html) >= 0){
-			$this->html = str_replace('@base', ($base == "" ? "" : '<base href="'.$base.'" />'), $this->html);
+		ZView::$dir = dirname((new ReflectionClass(get_called_class()))->getFilename());
+		ZView::$html = file_get_contents(ZView::$dir . DIRECTORY_SEPARATOR .static::$appFile);
+		preg_match_all('/\@include\([\'"]{1}([a-zA-Z\\\'"_]*)[\'"]{1}\)/', ZView::$html, $match);
+		ZView::loadContent($content, $match);
+		if(find('@base', ZView::$html) >= 0){
+			ZView::$html = str_replace('@base', ($base == "" ? "" : '<base href="'.$base.'" />'), ZView::$html);
 		}
-		while(preg_match('/\@yield\([\'"]{1}([a-zA-Z\\\'"_]*)[\'"]{1}\)/', $this->html, $match)){
-			$this->html = str_replace($match[0], $this->loadYield($match[1]), $this->html);
+		while(preg_match('/\@yield\([\'"]{1}([a-zA-Z\\\'"_]*)[\'"]{1}\)/', ZView::$html, $match)){
+			ZView::$html = str_replace($match[0], ZView::loadYield($match[1]), ZView::$html);
 		}
-		$this->html = ZBladeCompiler::compile($this->html, $data);
-	}
-
-	private function loadContent($name, $match){
-		$tmp = file_get_contents($this->dir . DIRECTORY_SEPARATOR . static::$viewDir . DIRECTORY_SEPARATOR .$name.(find(".", $name) < 0 ?".html" : ""));
-		for($i = 0; $i < sizeof($match[1]); $i++){
-			$t = strlen($this->html);
-			$this->html = str_replace("@include('".$match[1][$i]."')", get_string_between($tmp, "@".$match[1][$i], "@end".$match[1][$i]), $this->html);
-			if($t == strlen($this->html)){
-				$this->html = str_replace('@include("'.$match[1][$i].'")', get_string_between($tmp, "@".$match[1][$i], "@end".$match[1][$i]), $this->html);
-			}
-		}
-	}
-
-	private function loadYield($name){
-		return file_get_contents($this->dir . DIRECTORY_SEPARATOR . static::$viewDir . DIRECTORY_SEPARATOR .$name.(find(".", $name) < 0 ? ".html" : ""));
-	}
-
-	public function returnHtml(){
-		return $this->html;
-	}
-
-	public static function get($content, $base = null, $data = null){
-		return (new static($content, $base, $data))->returnHtml();
+		echo ZBladeCompiler::compile(ZView::$html, $data);
 	}
 
 }
