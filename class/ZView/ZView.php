e649b76f2155526c345a78d8b776f878fa341402
Zexal0807 <gallinar00@gmail.com>
2019-05-08 12:33:25 +0200

    Fix ZView

--- class/ZView/ZView.php
+++ class/ZView/ZView.php
@@ -19,7 +19,7 @@ class ZView{
 	}
 
 	private static function loadYield($name){
-		return file_get_contents(ZView::$dir . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . static::$viewDir . DIRECTORY_SEPARATOR .$name.(find(".", $name) < 0 ? ".html" : ""));
+		return file_get_contents(ZView::$dir . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . $name.(find(".", $name) < 0 ? ".html" : ""));
 	}
 
 	public static function get($content, $base = null, $data = null){
@@ -38,7 +38,7 @@ class ZView{
 			ZView::$html = str_replace('@base', ($base == "" ? "" : '<base href="'.$base.'" />'), ZView::$html);
 		}
 		while(preg_match('/\@yield\([\'"]{1}([a-zA-Z\\\'"_]*)[\'"]{1}\)/', ZView::$html, $match)){
-			ZView::$html = str_replace($match[0], ZView::loadYield($match[1]), ZView::$html);
+			ZView::$html = str_replace($match[0], ZView::loadYield(static::$viewDir . DIRECTORY_SEPARATOR . $match[1]), ZView::$html);
 		}
 		echo ZBladeCompiler::compile(ZView::$html, $data);
 	}
