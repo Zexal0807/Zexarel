bb6d1b7d939279425eb89e5856722cd8e0adcd92
Zexal0807 <gallinar00@gmail.com>
2019-06-04 16:44:46 +0200

    Create class ZIntervall

---
+++ class/ZTimeCompare/ZIntervall.php
@@ -0,0 +1,16 @@
+<?php
+class ZIntervall{
+	private $inizio;
+	private $fine;
+
+	public function __construct(){
+	}
+	public function getInizio(){
+		return $this->inizio;
+	}
+	public function getFine(){
+		return $this->fine;
+	}
+
+}
+?>
