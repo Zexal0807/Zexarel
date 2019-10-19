ad40fd5882a6db1a6bf2ffae95c1bb4fa964ae28
Zexal0807 <gallinar00@gmail.com>
2019-10-19 15:08:59 +0200

    Create Radio and Checkbox Question

---
+++ class/ZexalForm/ZCheckQuestion.php
@@ -0,0 +1,26 @@
+<?php
+class ZCheckQuestion extends ZQuestion{
+  private $data;
+
+  public function __construct($name, $domanda, $descrizione, $obbligatoria, $data){
+    $this->data = $data;
+    parent::__construct($name, $domanda, $descrizione, $obbligatoria, null);
+  }
+
+  public function getHtml(){
+    $html = $this->getPreHtml();
+    $html .= '<div class="question-check">';
+    foreach($this->data as $k => $v){
+      $html .='<div class="check-option">
+      <div class="outer-check">
+        <div class="inner-check"></div>
+      </div>
+      <div class="check-desc">'.$v.'</div>
+      <input class="anser" type="checkbox" name="'.$this->content['name'].'" value="'.$k.'">
+      </div>';
+    }
+    $html .= '</div>'.$this->getPostHtml();
+    return $html;
+  }
+
+}
---
+++ class/ZexalForm/ZRadioQuestion.php
@@ -0,0 +1,26 @@
+<?php
+class ZRadioQuestion extends ZQuestion{
+  private $data;
+
+  public function __construct($name, $domanda, $descrizione, $obbligatoria, $data){
+    $this->data = $data;
+    parent::__construct($name, $domanda, $descrizione, $obbligatoria, null);
+  }
+
+  public function getHtml(){
+    $html = $this->getPreHtml();
+    $html .= '<div class="question-radio">';
+    foreach($this->data as $k => $v){
+      $html .='<div class="radio-option">
+      <div class="outer-radio">
+        <div class="inner-radio"></div>
+      </div>
+      <div class="radio-desc">'.$v.'</div>
+      <input class="anser" type="radio" name="'.$this->content['name'].'" value="'.$k.'">
+      </div>';
+    }
+    $html .= '</div>'.$this->getPostHtml();
+    return $html;
+  }
+
+}
