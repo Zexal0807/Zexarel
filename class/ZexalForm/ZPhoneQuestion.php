71820fc0a6c0770ec7df94945663d9b2dd78f275
Zexal0807 <gallinar00@gmail.com>
2019-10-19 16:00:24 +0200

    Add phone number question

---
+++ class/ZexalForm/ZPhoneQuestion.php
@@ -0,0 +1,17 @@
+<?php
+class ZPhoneQuestion extends ZQuestion{
+
+  public function __construct($name, $domanda, $descrizione, $obbligatoria){
+    parent::__construct($name, $domanda, $descrizione, $obbligatoria, "([\+]\d{2})?([\ ])?\d{8,10}");
+  }
+
+  public function getHtml(){
+    $html = $this->getPreHtml();
+    $html .= '<div class="question-number">
+		  <input class="anser" name="'.$this->content['name'].'" type="tel" placeholder="La tua risposta">
+		<div class="underline"></div>
+		</div>';
+    $html .= $this->getPostHtml();
+    return $html;
+  }
+}
