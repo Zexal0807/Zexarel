08cd1e0d23898160e1d7d2150bb7ea3cb792cc22
Zexal0807 <gallinar00@gmail.com>
2023-07-30 15:38:42 +0200

    Update to php8

--- class/ZDocument/ZPdf.php
+++ class/ZDocument/ZPdf.php
@@ -1041,8 +1041,8 @@ class ZPdf{
 		if(ini_get('mbstring.func_overload') & 2)
 			$this->Error('mbstring overloading must be disabled');
 		// Ensure runtime magic quotes are disabled
-		if(get_magic_quotes_runtime())
-			@set_magic_quotes_runtime(0);
+		//if(get_magic_quotes_runtime())
+		//	@set_magic_quotes_runtime(0);
 	}
 
 	protected function _checkoutput()
@@ -1919,4 +1919,3 @@ class ZPdf{
 		$this->state = 3;
 	}
 }
-?>
